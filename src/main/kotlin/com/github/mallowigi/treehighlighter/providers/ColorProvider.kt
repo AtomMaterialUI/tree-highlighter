37d5e1aaeec8d141d3f8935cb80696bb5a85d27d
Elior <heliosaian@gmail.com>
2025-05-24 21:54:00 +0300

    Add editor tab color provider implementation
    Introduced a new editor tab color provider to manage tab colors based on the project and file context. This integrates with the existing project tree configuration state and ensures consistent color handling in the IDE.

---
+++ src/main/kotlin/com/github/mallowigi/treehighlighter/providers/ColorProvider.kt
@@ -0,0 +1,22 @@
+package com.github.mallowigi.treehighlighter.providers
+
+import com.github.mallowigi.treehighlighter.ProjectTreeConfigState
+import com.intellij.openapi.fileEditor.impl.EditorTabColorProvider
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import java.awt.Color
+
+internal class ColorProvider : EditorTabColorProvider, DumbAware {
+  override fun getEditorTabColor(project: Project, file: VirtualFile): Color? = getColor(project, file)
+
+  override fun getProjectViewColor(project: Project, file: VirtualFile): Color? = getColor(project, file)
+
+  private fun getColor(project: Project, file: VirtualFile): Color? {
+    val colorSettings = ProjectTreeConfigState.instance.colorList
+    println("ColorProvider.getColor: colorSettings = $colorSettings")
+
+    return Color.BLACK
+  }
+
+}
